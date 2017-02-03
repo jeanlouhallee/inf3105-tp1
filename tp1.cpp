ba07b65f7ca9ff775a9f70bda85aed49080dcf64
Étienne <etienne.b.car@gmail.com>
2017-02-03 15:04:49 -0500

    J'ai ajouté a la surcharge de istream une verification des 	variables de classes. Elle permet de facilement savoir si les 	valeur entrees sont valables
    	J'ai ajouté un vecteur static dans la classe patient qui prend
    	en note les noPatient pour eviter qu'il y ait une duplication du
    	meme noPatient ainsi que les structures necessaire pour verifier
    	si un noPatient en fait deja partie ainsi que la verification
    	dans istream
    
    	J'ai enlevé la surcharge de istream et ostream de la definition
    	de la classe parce que ca créait des problème avec l'utilisation
    	du vecteur statique de classe listeNoPatient qui doit etre
    	DECLARE	a l'exterieur de la classe

--- tp1.cpp
+++ tp1.cpp
@@ -1,7 +1,7 @@
 /*
 --------------------------------TASKLIST----------------------------------------
 --1) Modifier le constructeur pour qu'il s'assure que la priorite du patient
-     soit >= 1 et <= 5
+     soit >= 1 et <= 5 // DONE
 --2) Implementer ou trouver un algorithme de tri ordonner le vecteur de
      [0] : le plus prioritaire a
      [N-1] : le moins prioritaire
@@ -9,7 +9,7 @@
 --3) Redifinir les surcharges d'operateurs booleen pour implementer
      un algorithme NON naif
 --4) Modification du OUTPUT
---5) Lire correctement depuis un fichier //Done
+--5) Lire correctement depuis un fichier //DONE
 --6) Ajouter un destructeur
 --7) Trouver comment empecher que deux patient aille le meme noPatient
  -------------------------------------------------------------------------------
@@ -17,9 +17,11 @@
 
 #include <iostream>
 #include <fstream>
-#include <assert.h>
 #include <vector>
+#include <stdexcept>
 using namespace std;
+
+int findElem(const vector<int> v, int elem);
 /**
  * Classe Patient qui contient le numero de Patient, le temp attendu par le
  * patient ainsi que sa priorité.
@@ -31,8 +33,8 @@ class Patient {
           int _noPatient;
           int _tempAttente;
           int _priorite;
-
-        public:
+      public:
+          static vector<int> listeNoPatient;
           Patient(int noPatient = -1, int tempAttente = 0, int priorite = 5)
               : _noPatient(noPatient), _tempAttente(tempAttente), _priorite(priorite) {
           }
@@ -43,43 +45,56 @@ class Patient {
           void set_tempAttente(int tempAttente);
           void set_priorite(int priorite);
           friend bool operator< (const Patient &p1, const Patient &p2);
-      /*
+          friend istream& operator >> (istream& is, Patient& patient);
+          friend ostream& operator << (ostream& os, const Patient& patient);
+          /*
+          Surcharge naive de l'operateur <. Cette version ne prendra en consideration que la priorité et le temps d'attente
+          On suppose que le PREMIER element du vecteur est CELUI QUI DOIT PASSER EN PREMIER
+          On suppose que le DERNIER element du vecteur est CELUI QUI DOIT PASSER EN DERNIER
+          p1 < p2 : suppose que p1 est plus prioritaire que p2
+          */
+          bool operator< (const Patient &p2){
+              bool p1PlusPrioritairep2 = false;
+              if (_priorite < p2._priorite){
+                      p1PlusPrioritairep2 = true;
+              }
+              else {
+                      if (_priorite == p2._priorite){
+                          if (_tempAttente > p2._tempAttente){
+                              p1PlusPrioritairep2 = true;
+                          }
+                      }
+            }
+            return p1PlusPrioritairep2;
+        }
+};
+
+        vector<int> Patient::listeNoPatient;
+        /*
         Surcharge le l'operateur <<. A MODIFIER pour que ca conincide avec les specification du prof pour l'affichage
          */
-        friend ostream& operator << (ostream& os, const Patient& patient) {
+        ostream& operator << (ostream& os, const Patient& patient) {
           os << "No Patient : " << patient._noPatient << ", Temp attendu dans la salle d'attente (s) : " << patient._tempAttente << " Priorite : " << patient._priorite;
           return os;
         }
         /*
         Surcharge de l'operateur >>. Va nous permettre de lire les donnees facilement en ignorant les espaces
          */
-        friend istream& operator >> (istream& is, Patient& patient) {
-
-          if(is >> patient._noPatient){
-            is >> patient._tempAttente >> patient._priorite;
-          }
+       istream& operator >> (istream& is, Patient& patient) {
+          int noPatient, tempAttente, priorite;
+          if(is >> noPatient){
+            is >> tempAttente >> priorite;
+                if (noPatient < 0 || findElem(Patient::listeNoPatient, noPatient) != -1 || tempAttente < 1 || priorite < 2 || priorite > 5){
+                      throw invalid_argument("Erreur pendant la creation du patient");
+                } else {
+                      patient._noPatient = noPatient;
+                      patient._tempAttente = tempAttente;
+                      patient._priorite = priorite;
+                      Patient::listeNoPatient.push_back(noPatient);
+                }
+        }
           return is;
         }
-        /*
-        Surcharge naive de l'operateur <. Cette version ne prendra en consideration que la priorité et le temps d'attente
-        On suppose que le PREMIER element du vecteur est CELUI QUI DOIT PASSER EN PREMIER
-        On suppose que le DERNIER element du vecteur est CELUI QUI DOIT PASSER EN DERNIER
-        p1 < p2 : suppose que p1 est plus prioritaire que p2
-         */
-        bool operator< (const Patient &p2){
-            bool p1PlusPrioritairep2 = false;
-            if (_priorite < p2._priorite){
-                    p1PlusPrioritairep2 = true;
-            } else {
-                if (_priorite == p2._priorite){
-                  if (_tempAttente > p2._tempAttente){
-                      p1PlusPrioritairep2 = true;
-                  }
-              }
-            }
-            return p1PlusPrioritairep2;
-        }
-};
 
 /*
 Getters et setters. Principalement pour la forme.
@@ -104,13 +119,10 @@ void Patient::set_tempAttente(int tempAttente){
 void Patient::set_priorite(int priorite){
       _priorite = priorite;
 }
-/*
-modification
- */
 
 void readFile(char *arcv, vector<Patient> *vp){
     ifstream file(arcv);
-    if(file){
+      if(file){
         Patient patient;
         while(file >> patient){
             vp->push_back(patient);
@@ -120,6 +132,20 @@ void readFile(char *arcv, vector<Patient> *vp){
     }else{
         cout << "TENGO UN PROBLEMA SENOR" << endl;
     }
+}
+
+int findElem(const vector<int> v, int elem){
+  int pos = -1, i;
+  bool trouve = false;
+  if (!v.empty()){
+    for (i = 0; i < v.size() && !trouve; ++i){
+        if (v[i] == elem) {
+          trouve = true;
+          pos = i;
+        }
+    }
+  }
+  return pos;
 }
 
 
