a75c276f81c03c3e28353fb6dc8095c314346c93
Étienne <etienne.b.car@gmail.com>
2017-02-03 11:30:24 -0500

    Changement : 	1) -Ajout d'une tasklist 	2) -Implementation naive de patient 1 < patient 2 		Veut dire que p1 est plus prioritaire que p2

--- tp1.cpp
+++ tp1.cpp
@@ -1,3 +1,17 @@
+/*
+--------------------------------TASKLIST----------------------------------------
+--1) Modifier le constructeur pour qu'il s'assure que la priorite du patient
+     soit >= 1 et <= 5
+--2) Implementer ou trouver un algorithme de tri ordonner le vecteur de
+     [0] : le plus prioritaire a
+     [N-1] : le moins prioritaire
+     Side-Note : Changer vecteur pour une organisation en pile?
+--3) Redifinir les surcharges d'operateurs booleen pour implementer
+     un algorithme NON naif
+--4) Modification du OUTPUT
+ -------------------------------------------------------------------------------
+ */
+
 #include <iostream>
 #include <assert.h>
 #include <vector>
@@ -9,37 +23,59 @@ using namespace std;
  * des relations de grandeurs pour trier le vecteu qui va contenir les patients
  */
 class Patient {
-public:
-  Patient(int noPatient = -1, int tempAttente = 0, int priorite = 5)
-        : _noPatient(noPatient), _tempAttente(tempAttente), _priorite(priorite) {
-  }
-  int get_noPatient();
-  int get_tempAttente();
-  int get_priorite();
-  void set_noPatient(int noPatient);
-  void set_tempAttente(int tempAttente);
-  void set_priorite(int priorite);
-private:
-  int _noPatient;
-  int _tempAttente;
-  int _priorite;
-/*
-Surcharge le l'operateur <<. A MODIFIER pour que ca conincide avec les specification du prof pour l'affichage
- */
-friend ostream& operator << (ostream& os, const Patient& patient) {
-  os << "No Patient : " << patient._noPatient << ", Temp attendu dans la salle d'attente (s) : " << patient._tempAttente << " Priorite : " << patient._priorite;
-  return os;
-}
-/*
-Surcharge de l'operateur >>. Va nous permettre de lire les donnees facilement en ignorant les espaces
- */
-friend istream& operator >> (istream& is, Patient& patient) {
-  if(is >> patient._noPatient){
-    is >> patient._tempAttente >> patient._priorite;
-  }
-  return is;
-}
+        private:
+          int _noPatient;
+          int _tempAttente;
+          int _priorite;
+
+        public:
+          Patient(int noPatient = -1, int tempAttente = 0, int priorite = 5)
+              : _noPatient(noPatient), _tempAttente(tempAttente), _priorite(priorite) {
+          }
+          int get_noPatient();
+          int get_tempAttente();
+          int get_priorite();
+          void set_noPatient(int noPatient);
+          void set_tempAttente(int tempAttente);
+          void set_priorite(int priorite);
+          friend bool operator< (const Patient &p1, const Patient &p2);
+      /*
+        Surcharge le l'operateur <<. A MODIFIER pour que ca conincide avec les specification du prof pour l'affichage
+         */
+        friend ostream& operator << (ostream& os, const Patient& patient) {
+          os << "No Patient : " << patient._noPatient << ", Temp attendu dans la salle d'attente (s) : " << patient._tempAttente << " Priorite : " << patient._priorite;
+          return os;
+        }
+        /*
+        Surcharge de l'operateur >>. Va nous permettre de lire les donnees facilement en ignorant les espaces
+         */
+        friend istream& operator >> (istream& is, Patient& patient) {
+          if(is >> patient._noPatient){
+            is >> patient._tempAttente >> patient._priorite;
+          }
+          return is;
+        }
+        /*
+        Surcharge naive de l'operateur <. Cette version ne prendra en consideration que la priorité et le temps d'attente
+        On suppose que le PREMIER element du vecteur est CELUI QUI DOIT PASSER EN PREMIER
+        On suppose que le DERNIER element du vecteur est CELUI QUI DOIT PASSER EN DERNIER
+        p1 < p2 : suppose que p1 est plus prioritaire que p2
+         */
+        bool operator< (const Patient &p2){
+            bool p1PlusPrioritairep2 = false;
+            if (_priorite < p2._priorite){
+                    p1PlusPrioritairep2 = true;
+            } else {
+                if (_priorite == p2._priorite){
+                  if (_tempAttente > p2._tempAttente){
+                      p1PlusPrioritairep2 = true;
+                  }
+              }
+            }
+            return p1PlusPrioritairep2;
+        }
 };
+
 /*
 Getters et setters. Principalement pour la forme.
 A potentiellement enlever vu que le friendship avec << et >> nous permettent de
@@ -82,6 +118,8 @@ int main(int argc, char *arcv[]){
             salleAttente.push_back(p); // met le patient dans la salle d'attente
             cout << salleAttente[i] << endl; // Affiche le patient
         }
+        if (salleAttente[0] < salleAttente[1])
+          cout << "Patient 0 est plus prioritaire que patient 1" << endl;
         cout << argc << endl;
         cout << arcv[0] << endl;
         cout << "Fin du programme" << endl;
