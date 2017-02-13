3b9c0ca36d50eed03abca10593fc5d139ad68c69
Jean-Lou <jeanlou601@hotmail.com>
2017-02-13 16:41:30 -0500

    Statistiques fonctionnelles.

--- tp1.cpp
+++ tp1.cpp
@@ -179,37 +179,42 @@ void traiterSalleAttente(vector<Patient> &salleAttente, int delaiTraitement){
 
 void statistiques(vector<Patient> &sa, const int tab[]){
     int i = 0;
-    int nbDeux = 0, nbTrois = 0, nbQuatre = 0, nbCinq = 0;
-    int deuxNOk = 0, troisNOk = 0, quatreNOk = 0, cinqNOk = 0;
+    double nbDeux = 0, nbTrois = 0, nbQuatre = 0, nbCinq = 0;
+    double deuxOk = 0, troisOk = 0, quatreOk = 0, cinqOk = 0;
+
 
     for(i ; i < sa.size(); ++i){
     
         if(sa[i].get_priorite() == 2){
             ++nbDeux;
-            if( (tab[0] - sa[i].get_tempAttente()) < 0 ){
-                ++deuxNOk;
+            if( (tab[0] - sa[i].get_tempAttente()) >= 0 ){
+                ++deuxOk;
             }
         }else if(sa[i].get_priorite() == 3){
             ++nbTrois;
-            if( (tab[1] - sa[i].get_tempAttente()) < 0 ){
-                ++troisNOk;
+            if( (tab[1] - sa[i].get_tempAttente()) >= 0 ){
+                ++troisOk;
             }
 
         }else if(sa[i].get_priorite() == 4){
             ++nbQuatre;
-            if( (tab[2] - sa[i].get_tempAttente()) < 0 ){
-                ++quatreNOk;
+            if( (tab[2] - sa[i].get_tempAttente()) >= 0 ){
+                ++quatreOk;
             }
 
         }else if(sa[i].get_priorite() == 5){
             ++nbCinq;
-            if( (tab[3] - sa[i].get_tempAttente()) < 0 ){
-                ++cinqNOk;
+            if( (tab[3] - sa[i].get_tempAttente()) >= 0 ){
+                ++cinqOk;
             }
 
         }
     }
-
+    cout << sa[0].get_tempAttente() << endl;
+    cout << endl << nbDeux << endl << deuxOk << endl;
+    cout << "--------" << endl << "Priorite 2 " << "Priorite 3 " << "Priorite 4 "  << "Priorite 5" << endl;
+    cout << deuxOk/nbDeux  << "      " << troisOk/nbTrois << "      "  << quatreOk/nbQuatre  << "      " << cinqOk/nbCinq  << endl;
+    cout << (deuxOk/nbDeux) * (troisOk/nbTrois) * (quatreOk/nbQuatre) * (cinqOk/nbCinq) <<endl;
 }
 
 int main(int argc, char *arcv[]){
